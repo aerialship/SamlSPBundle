af36de9adab3947dc1ec7a9e64ae024fb1388e33
Milos Tomic <tmilos@gmail.com>
2014-02-27 10:31:41 +0100

    Authenticate fixed only to redirect binding and config settings ignored #23

--- src/AerialShip/SamlSPBundle/Bridge/Authenticate.php
+++ src/AerialShip/SamlSPBundle/Bridge/Authenticate.php
@@ -10,6 +10,7 @@ use AerialShip\SamlSPBundle\State\Request\RequestState;
 use AerialShip\SamlSPBundle\State\Request\RequestStateStoreInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Http\HttpUtils;
 
 
@@ -24,20 +25,26 @@ class Authenticate implements RelyingPartyInterface
     /** @var \Symfony\Component\Security\Http\HttpUtils  */
     protected $httpUtils;
 
+    /** @var \AerialShip\SamlSPBundle\Bridge\BindingManager  */
+    protected $bindingManager;
+
 
     /**
      * @param ServiceInfoCollection $serviceInfoCollection
      * @param RequestStateStoreInterface $requestStore
      * @param HttpUtils $httpUtils
+     * @param BindingManager $bindingManager
      */
     public function __construct(
         ServiceInfoCollection $serviceInfoCollection,
         RequestStateStoreInterface $requestStore,
-        HttpUtils $httpUtils
+        HttpUtils $httpUtils,
+        BindingManager $bindingManager
     ) {
         $this->serviceInfoCollection = $serviceInfoCollection;
         $this->requestStore = $requestStore;
         $this->httpUtils = $httpUtils;
+        $this->bindingManager = $bindingManager;
     }
 
 
@@ -77,16 +84,22 @@ class Authenticate implements RelyingPartyInterface
         $builder = new AuthnRequestBuilder($spED, $idpED, $spMeta);
         $message = $builder->build();
 
-        $binding = new HttpRedirect();
-        /** @var $bindingResponse \AerialShip\LightSaml\Binding\RedirectResponse */
+        $binding = $this->bindingManager->instantiate($spMeta->getAuthnRequestBinding());
+
         $bindingResponse = $binding->send($message);
+        if ($bindingResponse instanceof \AerialShip\LightSaml\Binding\RedirectResponse) {
+            $result = new RedirectResponse($bindingResponse->getUrl());
+        } else if ($bindingResponse instanceof \AerialShip\LightSaml\Binding\PostResponse) {
+            $result = new Response($bindingResponse->render());
+        } else {
+            throw new \RuntimeException('Unrecognized binding response '.get_class($bindingResponse));
+        }
 
         $state = new RequestState();
         $state->setId($message->getID());
         $state->setDestination($serviceInfo->getIdpProvider()->getEntityDescriptor()->getEntityID());
         $this->requestStore->set($state);
 
-        $result = new RedirectResponse($bindingResponse->getUrl());
         return $result;
     }
 
