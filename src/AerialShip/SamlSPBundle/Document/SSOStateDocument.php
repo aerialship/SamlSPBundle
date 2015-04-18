8291cf2162b066ebb0d8df18e04c717d49ec86df
Bernardo Silva <benny.stuff@gmail.com>
2015-04-18 14:23:07 +0100

    Add SSOStateDocument to be used by doctrine odm.

---
+++ src/AerialShip/SamlSPBundle/Document/SSOStateDocument.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace AerialShip\SamlSPBundle\Document;
+
+use AerialShip\SamlSPBundle\State\SSO\SSOState;
+use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+
+
+abstract class SSOStateDocument extends SSOState
+{
+    /**
+     * @var string
+     * @ODM\String(name="provider_id")
+     */
+    protected $providerID;
+
+    /**
+     * @var string
+     * @ODM\String(name="auth_svc_name")
+     */
+    protected $authenticationServiceName;
+
+    /**
+     * @var string
+     * @ODM\String(name="session_index")
+     */
+    protected $sessionIndex;
+
+    /**
+     * @var string
+     * @ODM\String(name="name_id")
+     */
+    protected $nameID;
+
+    /**
+     * @var string
+     * @ODM\String(name="name_id_format")
+     */
+    protected $nameIDFormat;
+
+    /**
+     * @var \DateTime
+     * @ODM\Date(name="created_on")
+     */
+    protected $createdOn;
+
+
+    /**
+     * @param \DateTime $createdOn
+     */
+    public function setCreatedOn($createdOn)
+    {
+        $this->createdOn = $createdOn;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedOn()
+    {
+        return $this->createdOn;
+    }
+}
